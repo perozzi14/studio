8e385aef7db0a905c23c8089a8aa38e20c9db028
Salvatore nacario Perozzi Álvarez <perozzi0112@gmail.com>
2025-07-08 23:30:14 +0000

    starting build "2d88ab51-2bd4-48bb-8632-67a6a2024528" FETCHSOURCE BUILD

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,16 +1,14 @@
 
-import Link from 'next/link'
-
 export default function NotFound() {
   return (
-    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', padding: '4rem', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: '100vh' }}>
-        <h1 style={{ fontSize: '2rem', fontWeight: 'bold', marginBottom: '1rem' }}>404 - Página No Encontrada</h1>
+    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', padding: '4rem', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: '100vh', backgroundColor: '#F0F8FF' }}>
+        <h1 style={{ fontSize: '2rem', fontWeight: 'bold', marginBottom: '1rem', color: '#333' }}>404 - Página No Encontrada</h1>
         <p style={{ maxWidth: '450px', marginBottom: '2rem', color: '#666' }}>
             Lo sentimos, la página que estás buscando no existe o ha sido movida.
         </p>
-        <Link href="/" style={{ textDecoration: 'none', color: 'white', backgroundColor: '#7EC4CF', padding: '0.75rem 1.5rem', borderRadius: '0.5rem' }}>
+        <a href="/" style={{ textDecoration: 'none', color: 'white', backgroundColor: '#7EC4CF', padding: '0.75rem 1.5rem', borderRadius: '0.5rem' }}>
             Volver a la Página de Inicio
-        </Link>
+        </a>
     </div>
   )
 }
