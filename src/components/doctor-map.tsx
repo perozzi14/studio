aa04339c3dbd3bc95136bd643c5a76dac91d2ddc
Salvatore nacario Perozzi Álvarez <perozzi0112@gmail.com>
2025-07-08 19:46:52 +0000

    starting build "f307694e-0da0-4e23-8ce6-5cdc084134d3" FETCHSOURCE BUILD

--- src/components/doctor-map.tsx
+++ src/components/doctor-map.tsx
@@ -3,7 +3,7 @@
 
 import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import type { Doctor } from "@/lib/data";
+import type { Doctor } from "@/lib/types";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import Image from "next/image";
